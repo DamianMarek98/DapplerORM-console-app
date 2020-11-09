91d48ebccf408b7bce49613f1e2f253391844ed9
Damian Marek <zmdddamian@gmail.com>
2020-11-09 23:35:37 +0100

    added simple services

---
+++ NHibernate/NHibernate/Services/BaseRepo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+using Dapper;
+
+namespace NHibernate.Services
+{
+    public class BaseRepo
+    {
+        public static string DbFIle
+        {
+            get => Environment.CurrentDirectory + "\\..\\SimpleDb.sqlite";
+        }
+
+        public static SQLiteConnection DbConnection()
+        {
+            return new SQLiteConnection("Data Source=" + DbFIle);
+        }
+
+        public static void CreateDatabase()
+        {
+            if (File.Exists(BaseRepo.DbFIle))
+            {
+                using (var cnn = DbConnection())
+                {
+                    cnn.Open();
+                    cnn.Execute("DROP table Object;");
+                    cnn.Execute("DROP table PhysicalClient;");
+                    cnn.Execute("DROP table InternetClient;");
+                    
+                }
+            }
+            
+            using (var cnn = DbConnection())
+            {
+                cnn.Open();
+                cnn.Execute(@"create table Object
+                (
+                    ID                                  INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Description                           varchar(100),
+                    Price                            integer,
+                    InStock                         integer
+                )");
+                
+                cnn.Execute(@"create table PhysicalClient
+                (
+                    ID                                  INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Name                                varchar(100),
+                    Address                             varchar(100)
+                )");
+                
+                cnn.Execute(@"create table InternetClient
+                (
+                    ID                                  INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Name                                varchar(100),
+                    Address                             varchar(100),
+                    IpAddress                           varchar(100)
+                )");
+            }
+        }
+    }
+}
---
+++ NHibernate/NHibernate/Services/ClientService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Dapper;
+using NHibernate.Mapping;
+using NHibernate.Model;
+using NHibernate.Repo;
+
+namespace NHibernate.Services
+{
+    public class ClientService
+    {
+        private readonly PhysicalClientRepository _physicalClientRepository = new PhysicalClientRepository();
+        private readonly InternetClientRepository _internetClientRepository = new InternetClientRepository();
+
+        public List<Tuple<Int64, string>> GetAllClientsWithNameContaining(string str)
+        {
+            if (!File.Exists(BaseRepo.DbFIle))
+            {
+                BaseRepo.CreateDatabase();
+            }
+
+            using (var cnn = BaseRepo.DbConnection())
+            {
+                cnn.Open();
+                string like = "'%" + str + "%'";
+                string sql =
+                    "SELECT Id, Name FROM InternetClient WHERE Name LIKE " + like 
+                     + " UNION ALL"                                                                           
+                     + " SELECT Id, Name  From PhysicalClient  WHERE Name LIKE " + like 
+                     + " ORDER BY Name"
+                    + " LIMIT 10;";
+                   
+
+                return cnn.Query<Int64, string, Tuple<Int64, string>>(sql, System.Tuple.Create, splitOn: "*").AsList();
+            }
+            
+            
+        }
+    }
+}
